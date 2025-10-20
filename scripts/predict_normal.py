c59bd37aa0481b5ac8da2bdf4336be3cd16c8381
legency <lichen900210@gmail.com>
2025-10-20 17:18:56 +0800

    fix remove crop

--- scripts/predict_normal.py
+++ scripts/predict_normal.py
@@ -40,8 +40,7 @@ def predict_image(image_path):
 
     # 定义图像的预处理步骤
     preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
